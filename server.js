f63ba2edd2d9d4097979ccd3449a7c46a3d9d48a
MartiPuigV <119410972+MartiPuigV@users.noreply.github.com>
2024-12-21 21:22:52 +0100

    basic html template going on

---
+++ server.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.POSTGRES_EXTERNAL_URL,
+    port: parseInt(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+});
+
+module.exports = pool;
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error connecting to the database', err);
+    } else {
+        console.log('Connected to the database');
+    }
+});
